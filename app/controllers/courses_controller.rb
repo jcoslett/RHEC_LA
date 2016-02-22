6dd124822bd5d680ac2db40ee79cf28956eea0ef
James Coslett <james.coslett@gmail.com>
2016-02-22 12:09:19 -0800

    Got new and create working

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,11 +1,25 @@
 class CoursesController < ApplicationController
 
-def index
-  @courses = Course.all
-end
+  def index
+    @courses = Course.all
+  end
 
-def show
-  @course = Course.find(params[:id])
-end
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(params.require(:course).permit(:category, :name, :course_id))
+
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
 
   get "courses/" => "courses#index"
+  get "courses/new" => "courses#new", as: :new_course
   get "courses/:id" => "courses#show", as: :course
+  post "courses/" => "courses#create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
